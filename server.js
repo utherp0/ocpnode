31b874e749ca0d0cf0091494560830a1478d5fe8
Ian 'Uther' Lawson <ian.lawson@redhat.com>
2019-08-13 12:54:55 +0100

    Added database name

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get('/dbcreate', function (req,res)
   console.log("Connection info set to " + dbhost + ":" + dbuser + ":" + dbpassword );
 
   //var connection = mysql.createConnection({host:dbhost,user:dbuser,password:dbpassword,database:sampledb});
-  var connection = mysql.createConnection({host:dbhost,user:dbuser,password:dbpassword});
+  var connection = mysql.createConnection({host:dbhost,user:dbuser,password:dbpassword,database:sampledb});
   console.log("Created connection object....");
 
   connection.connect(function(err)
